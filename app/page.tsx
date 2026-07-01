521b826875d613439c4fd26b9a80650996bfd10a
nationtechoffice <nationtechoffice@gmail.com>
2026-06-30 23:19:50 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
 export default function Page() {
-  return (
-    <div style={{ padding: '50px', color: 'white', background: 'black', minHeight: '100vh' }}>
-      <h1>Hello World - Testing if Page loads</h1>
-    </div>
-  )
+  return <h1>Hello World</h1>
 }
